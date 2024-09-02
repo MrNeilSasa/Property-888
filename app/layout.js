f80e2828248482179426e28fb1d872989cf5235e
Antonio Neil <bossneil20@gmail.com>
2024-09-01 21:11:06 -0500

    favicon for search engine

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { SessionProvider } from "next-auth/react";
-import { ThirdwebProvider } from "@/app/thirdweb";
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { SessionProvider } from 'next-auth/react'
+import { ThirdwebProvider } from '@/app/thirdweb'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Property888",
+  title: 'Property888',
   description:
-    "Property 888 is a Blockchain technology, decentralized application (DApp), that incorporates Artificial Intelligence, for the design and eventual build out of high quality modern architecture residential properties. Property 888 allows for the tokenization of real estate, with NFT property loans at 0% interest, with liquidity providers obtaining 12% rewards in US$C, or mUS$C tokens.",
-};
+    'Property 888 is a Blockchain technology, decentralized application (DApp), that incorporates Artificial Intelligence, for the design and eventual build out of high quality modern architecture residential properties. Property 888 allows for the tokenization of real estate, with NFT property loans at 0% interest, with liquidity providers obtaining 12% rewards in US$C, or mUS$C tokens.',
+  icons: {
+    icon: './favicon.ico',
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -26,5 +29,5 @@ export default function RootLayout({ children }) {
         </SessionProvider>
       </ThirdwebProvider>
     </html>
-  );
+  )
 }
